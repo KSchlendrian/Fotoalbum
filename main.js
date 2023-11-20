7ac89a9902163f4660b60e8e945d7cfc8bf15b08
KSchlendrian <cbii@hotmail.de>
2023-11-20 11:18:19 +0100

    v0.5

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const imageSources = ['./images/alpakas.jpg', './images/eichhoernchen.jpg', './images/elefanten.jpg', './images/esel.jpg', './images/eule.jpg', './images/frosch.jpg', './images/gecko.jpg', './images/geparden.jpg', './images/kaninchen.jpg', './images/lamas.jpg', './images/makaks.jpg', './images/mobs.jpg', './images/nilpferde.jpg', './images/panda.jpg', './images/papagei.jpg', './images/seehund.jpg', './images/tucan.jpg', './images/wolf.jpg'];
-
   const gallery = document.getElementById('gallery');
-
-  const prevPageButton = document.getElementById('prevPage');
-  const nextPageButton = document.getElementById('nextPage');
 
   function showImagesForPage(pageIndex, imagesPerPage) {
     gallery.innerHTML = '';
@@ -17,22 +13,70 @@ document.addEventListener('DOMContentLoaded', function () {
       img.classList.add('image');
       img.src = imageSources[i];
       img.alt = `Image ${i + 1}`;
+      img.draggable = true;
+      img.id = `image-${i + 1}`;
       gallery.appendChild(img);
     }
+
+    const images = document.querySelectorAll('.image');
+    images.forEach((img) => {
+      img.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('text/plain', img.id);
+      });
+      img.addEventListener('dragover', (event) => {
+        event.preventDefault();
+      });
+      img.addEventListener('dragenter', (event) => {
+        event.preventDefault();
+      });
+      img.addEventListener('drop', onDrop);
+    });
   }
 
   showImagesForPage(0, 8);
 
-  let currentPage = 0;
-
-  nextPageButton.addEventListener('click', function () {
-    currentPage++;
-    showImagesForPage(currentPage, 8);
+  const draggableElement = document.querySelectorAll('.drag');
+  draggableElement.forEach((draggableElement) => {
+    draggableElement.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', draggableElement.textContent);
+    });
   });
-  prevPageButton.addEventListener('click', function () {
-    if (currentPage > 0) {
-      currentPage--;
-      showImagesForPage(currentPage, 8);
-    }
+
+  const dropElement = document.querySelectorAll('.drop-target');
+  dropElement.forEach((dropElement) => {
+    dropElement.addEventListener('dragenter', (event) => {
+      event.preventDefault();
+    });
+
+    dropElement.addEventListener('dragover', (event) => {
+      event.preventDefault();
+    });
+
+    dropElement.addEventListener('drop', onDrop);
   });
 });
+
+function onDrop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData('text/plain');
+  const newParagraph = document.createElement('p');
+  const droppedElement = document.getElementById(data);
+  const dropTarget = event.target;
+
+  newParagraph.textContent = data;
+  event.target.appendChild(newParagraph);
+
+  if (droppedElement) {
+    const newImage = document.createElement('img');
+    newImage.src = droppedElement.src;
+    newImage.alt = droppedElement.alt;
+    newImage.classList.add('image');
+
+    const currentImage = dropTarget.querySelector('.image');
+    if (currentImage) {
+      dropTarget.removeChild(currentImage);
+    }
+
+    dropTarget.appendChild(newImage);
+  }
+}
